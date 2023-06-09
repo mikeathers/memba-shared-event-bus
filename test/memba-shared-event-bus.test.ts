aa053cb69096e4c371b3029a583768dfc6b808fb
Mike Atherton <athers_05@hotmail.co.uk>
2023-06-09 15:18:06 +0100

    Initial commit

---
+++ test/memba-shared-event-bus.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MembaSharedEventBus from '../lib/memba-shared-event-bus-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/memba-shared-event-bus-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MembaSharedEventBus.MembaSharedEventBusStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
